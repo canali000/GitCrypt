379e7608596512495bbd2d6a528160338607a32b
halilardaculha <230601018@bakircay.edu.tr>
2024-03-13 23:30:06 +0300

    halil arda sifreleme guncelleme

--- halilarda.cpp
+++ halilarda.cpp
@@ -1,52 +1,66 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <string>
 #include <cstdlib>
-#include <time.h>
+#include <ctime>
 
-int main()
-{
-	char sifreleme[4] = "on";
-    char anahtar[5000] = "asfhudsf4775214sfsdaf65244s2f1a6dgds46a5fds1rh8y4rets21fa654fdw8r42f1vds6f54e8yt4sd1gf3da1fa6w8r4h2123daf1as8fw6ras2df1e45tga6s5dsaf1212g4jhbfhjaduhaoiwfkasjf98y9287hrisaf7821sf2134ucfsd";
-	srand(static_cast<unsigned int>(time(0)));
+using namespace std;
 
-	if (strcmp(sifreleme, "on") == 0)
-	{   		
-		FILE *dosya = fopen("sifreleme.txt", "r");
-        char metin[5000];
-		fgets(metin, 5000, dosya);
-		dosya = fopen("sifreleme.txt", "w");
+int main(int argc, char *argv[]) {
+    string dosyad = string(argv[1]);
+    string sifreleme = string(argv[2]);
+    cout << dosyad << sifreleme;
+    string anahtar = "asfhudsf4775214sfsdaf65244s2f1a6dgds46a5fds1rh8y4rets21fa654fdw8r42f1vds6f54e8yt4sd1gf3da1fa6w8r4h2123daf1as8fw6ras2df1e45tga6s5dsaf1212g4jhbfhjaduhaoiwfkasjf98y9287hrisaf7821sf2134ucfsd";
+    srand(static_cast<unsigned int>(time(0)));
+ 
+    if (sifreleme == "on") {
+        ifstream dosya(dosyad);
+        if (!dosya) {
+            cout << "Dosya bulunamadi." << endl;
+            return 1;
+        }
+
+        string metin;
+        getline(dosya, metin);
+        dosya.close();
+
+        ofstream outFile(dosyad);
         int sifreleyici;
-		for (int i = 0; i < strlen(metin) * 5; i++)
-		{
-			if(i % 5 == 0){
-            char arr_anahtar = anahtar[(i / 5) % (strlen(anahtar) / 7)];
-			sifreleyici = metin[i / 5] + arr_anahtar;
-			fprintf(dosya, "%c", sifreleyici);
-		}
-        else{
-         sifreleyici = (rand() % 150) + 64;
-         fprintf(dosya, "%c", sifreleyici);
-        }}
-		fclose(dosya);
-	}
-	else if(strcmp(sifreleme, "off") == 0){
-		FILE *dosya = fopen("sifreleme.txt", "r");
-		char sifreMetni[5000];
-		fgets(sifreMetni, 5000, dosya);
-		int sifreleyici;
-		dosya = fopen("sifreleme.txt", "w");
+        for (int i = 0; i < metin.length() * 5; i++) {
+            if (i % 5 == 0) {
+                char arr_anahtar = anahtar[(i / 5) % (anahtar.length() / 7)];
+                sifreleyici = metin[i / 5] + arr_anahtar;
+                outFile << static_cast<char>(sifreleyici);
+            } else {
+                sifreleyici = (rand() % 150) + 64;
+                outFile << static_cast<char>(sifreleyici);
+            }
+        }
+        outFile.close();
+    } else if (sifreleme == "off") {
+        ifstream dosya(dosyad);
+        if (!dosya) {
+            cout << "Dosya bulunamadi." << endl;
+            return 1;
+        }
 
-		for (int i = 0; i < strlen(sifreMetni) / 5; i++)
-		{
-			char arr_anahtar = anahtar[i % (strlen(anahtar) / 7)];
-			sifreleyici = sifreMetni[i * 5] - arr_anahtar;
-			fprintf(dosya, "%c", sifreleyici);
-		}
-		fclose(dosya);
-	}
-	else{
-		printf("Lutfen gecerli bir arguman giriniz. (On / Off)");
-	}	
- return 0;
+        string sifreMetni;
+        getline(dosya, sifreMetni);
+        dosya.close();
+
+        ofstream outFile(dosyad);
+        int sifreleyici;
+        for (int i = 0; i < sifreMetni.length() / 5; i++) {
+            char arr_anahtar = anahtar[i % (anahtar.length() / 7)];
+            sifreleyici = sifreMetni[i * 5] - arr_anahtar;
+            outFile << static_cast<char>(sifreleyici);
+        }
+        outFile.close();
+    } else {
+        cout << "Lutfen gecerli bir arguman giriniz. (on / off)" << endl;
+        return 1;
+    }
+   
+
+    return 0;
 }
